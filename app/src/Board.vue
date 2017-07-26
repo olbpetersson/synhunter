ce72c4067a04fa85e24d95c1e9eb0e8eea8c4683
Marcus Hultman <hultman@spotify.com>
2017-07-26 15:47:08 +0200

    app: websocket initial connection

--- app/src/Board.vue
+++ app/src/Board.vue
@@ -3,6 +3,9 @@
   <md-layout v-for="(row, i) in parseInt(height)" class="row">
     <md-layout v-for="(tile, j) in parseInt(width)" md-gutter md-align="center">
       <md-card>
+        <md-card-actions>
+          <md-button @click="ping(i, j)">Ping</md-button>
+        </md-card-actions>
         <md-card-content>
           {{i}} {{j}}
         </md-card-content>
@@ -12,15 +15,35 @@
 </div>
 </template>
 <script>
+// const BASE_URL = 'ws://synhunter.mirkk.eu:1337';
+const BASE_URL = 'ws://54.171.223.125:1337';
+
 export default {
   data() {
-    return {};
+    return {
+      socket: null
+    };
   },
   props: ['width', 'height'],
   created() {
     console.log('Board initialized', this.width, this.height);
+    this.socket = new WebSocket(BASE_URL);
+
+    // this.socket.onopen = () => this.socket.send('Ping');
+    this.socket.onmessage = (e) => console.log('Server:', e);
   },
-  methods: {}
+  methods: {
+    ping(i, j) {
+      console.log('pinging...');
+      let payload = {
+        jsonrpc: "2.0",
+        method: 'foo',
+        params: [(i*parseInt(this.height)) + j],
+        id: 0
+      };
+      this.socket.send(JSON.stringify(payload));
+    }
+  }
 };
 </script>
 <style>
