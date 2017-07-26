bd0eae383799c1a595335c4b85fda03ab10fb33b
Marcus Hultman <hultman@spotify.com>
2017-07-26 16:11:02 +0200

    app: add game input

---
+++ app/src/GameInput.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <form novalidate @submit.stop.prevent="submit">
+    <md-input-container>
+      <label>{{label}}</label>
+      <md-input v-model="value"></md-input>
+    </md-input-container>
+    <md-button type="submit" class="md-raised">Submit</md-button>
+  </form>
+</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      value: null
+    };
+  },
+  props: ['label'],
+  created() {},
+  methods: {
+    submit() {
+      // todo: send to server
+    }
+  }
+};
+</script>
+<style>
+</style>
--- app/src/index.js
+++ app/src/index.js
@@ -4,10 +4,11 @@ import Vue from 'vue';
 import VueMaterial from 'vue-material';
 
 import Board from 'Board.vue';
+import GameInput from 'GameInput.vue';
 
 Vue.use(VueMaterial);
 
 const app = new Vue({
   el: '#app',
-  components: {Board}
+  components: {Board, GameInput}
 });
