42e3a50940d4c44bbc4fe2d42c273244c9565991
Marcus Hultman <hultman@spotify.com>
2017-07-26 14:55:13 +0200

    app: add main Board

---
+++ app/src/Board.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <md-layout v-for="(row, i) in parseInt(height)" class="row">
+    <md-layout v-for="(tile, j) in parseInt(width)" md-gutter md-align="center">
+      <md-card>
+        <md-card-content>
+          {{i}} {{j}}
+        </md-card-content>
+      </md-card>
+    </md-layout>
+  </md-layout>
+</div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: ['width', 'height'],
+  created() {
+    console.log('Board initialized', this.width, this.height);
+  },
+  methods: {}
+};
+</script>
+<style>
+.row {
+  padding-top: 12px;
+}
+</style>
--- app/src/index.js
+++ app/src/index.js
@@ -1,1 +1,13 @@
-console.log('hello, world')
+import 'vue-material-css';
+
+import Vue from 'vue';
+import VueMaterial from 'vue-material';
+
+import Board from 'Board.vue';
+
+Vue.use(VueMaterial);
+
+const app = new Vue({
+  el: '#app',
+  components: {Board}
+});
